b481b363ef65e7bf9da80c5b3ad6d19ea03cc342
Dave Tapson <dave.tapson@gmail.com>
2021-08-18 22:49:06 +1200

    Project finalized to end of course

---
+++ ContosoCrafts.WebSite/Controllers/ProductsController.cs
@@ -0,0 +1,34 @@
+﻿using ContosoCrafts.WebSite.Models;
+using ContosoCrafts.WebSite.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace ContosoCrafts.WebSite.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        public ProductsController(JsonFileProductService poductService)
+        {
+            ProductService = poductService;
+        }
+
+        public JsonFileProductService ProductService { get; }
+
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return ProductService.GetProducts();
+        }
+
+        //[HttpPatch] "[FromBody]" because we just want to use query string to test...
+        [Route("Rate")]
+        [HttpGet]
+        public ActionResult Get([FromQuery] string ProductId, int Rating)
+        {
+            ProductService.AddRating(ProductId, Rating);
+            return Ok();
+        }
+    }
+}
